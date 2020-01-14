93631dbd0119f99e2ecb048fec5a13b689d8204a
Martino Vallara <martino.vallara@xpeppers.com>
2020-01-14 15:57:34 +0100

    first commit

---
+++ src/test/java/it/vallara/saletaxesproblem/WorkingProjectTest.java
@@ -0,0 +1,13 @@
+package it.vallara.saletaxesproblem;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class WorkingProjectTest {
+
+    @Test
+    public void name() {
+        assertThat(true, is(true));
+    }
+}
