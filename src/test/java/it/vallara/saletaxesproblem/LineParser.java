6b313b232b6afa97938be30461bc63aa92c6ac10
Martino Vallara <martino.vallara@xpeppers.com>
2020-01-30 15:39:21 +0100

    let the parser build a taxed product

--- src/test/java/it/vallara/saletaxesproblem/LineParser.java
+++ src/test/java/it/vallara/saletaxesproblem/LineParser.java
@@ -9,15 +9,15 @@ import java.util.stream.Collectors;
 public class LineParser {
 
 
-    public Product parse(String line)
-    {
-        List<String> lineArgs = Arrays.asList( line.split(" "));
+    public Product parse(String line) {
+        List<String> lineArgs = Arrays.asList(line.split(" "));
         int quantity = Integer.parseInt(lineArgs.get(0));
         List<String> wordWithoutQuantity = new ArrayList(lineArgs.stream().skip(1).collect(Collectors.toList()));
         String productWithoutQuantity = String.join(" ", wordWithoutQuantity);
         String description = productWithoutQuantity.split("at ")[0].trim();
         double price = Double.parseDouble(line.split("at ")[1]);
 
+        if (description.equals("music CD")) return new Product(quantity, description, price, 0.1);
         return new Product(quantity, description, price);
     }
 }
--- src/test/java/it/vallara/saletaxesproblem/LineParserTest.java
+++ src/test/java/it/vallara/saletaxesproblem/LineParserTest.java
@@ -3,10 +3,12 @@ package it.vallara.saletaxesproblem;
 import org.hamcrest.core.Is;
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.assertThat;
 
 public class LineParserTest {
     LineParser parser;
+
     @Before
     public void setUp() {
         parser = new LineParser();
@@ -15,17 +17,26 @@ public class LineParserTest {
     @Test
     public void should_parse_quantity_description_with_space_and_price() {
         //todo: Attezione "music CD" ha le tasse!
-        Product product = parser.parse("1 music CD at 14.99");
-        Product expectedProduct = new Product(1, "music CD", 14.99);
+        Product product = parser.parse("1 book at 14.99");
+        Product expectedProduct = new Product(1, "book", 14.99);
 
         assertThat(expectedProduct.toString(), Is.is(product.toString()));
     }
 
     @Test
     public void should_parse_quantity_two_digits_description_a_word_and_price() {
-        Product product = parser.parse("10 music CD at 100.88");
-        Product expectedProduct = new Product(10, "music CD", 100.88);
-
+        Product product = parser.parse("10 book at 100.88");
+        Product expectedProduct = new Product(10, "book", 100.88);
+//todo: invertire parametri dell'assert
         assertThat(expectedProduct.toString(), Is.is(product.toString()));
     }
+
+    @Test
+    public void should_create_a_product_with_tax_rate() {
+        Product product = parser.parse("10 music CD at 5");
+        Product expectedProduct = new Product(10, "music CD", 5.0, 0.1);
+
+        assertThat(product.toString(), Is.is(expectedProduct.toString()));
+    }
+
 }
