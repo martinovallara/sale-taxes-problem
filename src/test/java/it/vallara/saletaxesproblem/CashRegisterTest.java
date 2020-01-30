fafbeb82e93f914bd30f1f7d2b84d3e51c0c1d87
Martino Vallara <martino.vallara@xpeppers.com>
2020-01-30 12:28:36 +0100

    Enable AT

--- src/test/java/it/vallara/saletaxesproblem/CashRegisterTest.java
+++ src/test/java/it/vallara/saletaxesproblem/CashRegisterTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.assertThat;
 public class CashRegisterTest {
 
     @Test
-    @Ignore
     public void should_print_receipt_without_taxes_for_a_book() {
         String input = "1 book at 10.00";
 
