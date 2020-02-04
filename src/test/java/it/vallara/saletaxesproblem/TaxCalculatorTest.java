0da8931e8887c961535c28f62cd0bf69321d4759
Martino Vallara <martino.vallara@xpeppers.com>
2020-02-04 18:26:55 +0100

    refactoring: move production class to main folder

---
+++ src/test/java/it/vallara/saletaxesproblem/TaxCalculatorTest.java
@@ -0,0 +1,20 @@
+package it.vallara.saletaxesproblem;
+
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import static it.vallara.saletaxesproblem.ProductTest.BuildProduct;
+import static org.junit.Assert.*;
+
+public class TaxCalculatorTest {
+
+/*
+    @Test
+    public void should_create_a_product_with_tax_rate() {
+        Product product = parser.parse("10 music CD at 5");
+        Product expectedProduct = BuildProduct( 10, "music CD", 5.0);
+
+        assertThat(product.toString(), Is.is(expectedProduct.toString()));
+    }
+*/
+}
